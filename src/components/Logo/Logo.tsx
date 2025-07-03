3a1f3004dde3d416f9c9fdd954b5428797bead90
la-niina <la.niina.me@gmail.com>
2025-07-03 15:36:54 +0300

    fix: update alt text for logo image to improve accessibility

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -16,7 +16,7 @@ export const Logo = (props: Props) => {
     return (
         /* eslint-disable @next/next/no-img-element */
         <img
-            alt="logo"
+            alt="mavin logo"
             width={193}
             height={34}
             loading={loading}
