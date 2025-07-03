a3643a8140eb5d1767270d0360ffcf450b7e9ce1
la-niina <la.niina.me@gmail.com>
2025-07-03 14:11:11 +0300

    feat: rename scripts for clarity and add S3ClientUploadHandler to importMap

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -295,6 +295,7 @@ export interface Media {
     };
     [k: string]: unknown;
   } | null;
+  prefix?: string | null;
   folder?: (string | null) | FolderInterface;
   updatedAt: string;
   createdAt: string;
@@ -1444,6 +1445,7 @@ export interface UsersSelect<T extends boolean = true> {
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
   caption?: T;
+  prefix?: T;
   folder?: T;
   updatedAt?: T;
   createdAt?: T;
